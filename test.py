9d146c1d74e148bf70f4ed94642268f112bdd6b0
Jari Bakken <jari.bakken@gmail.com>
2018-12-22 14:59:58 +0100

    remove test script

--- test.py
+++
@@ -1,7 +0,0 @@
-from spacy.strings import StringStore
-
-stringstore = StringStore()
-stringstore.add(u'rigga')
-apple_hash = stringstore[u'rigga']
-assert apple_hash == 17293262079951405058
-assert stringstore[apple_hash] == u'rigga'
