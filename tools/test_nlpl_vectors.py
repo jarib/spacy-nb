988d91d0f53300e4329db66a371ea6f1f3e260a2
Jari Bakken <jari.bakken@gmail.com>
2018-12-25 12:08:38 +0100

    Try only NNC vectors for now

--- tools/test_nlpl_vectors.py
+++ tools/test_nlpl_vectors.py
@@ -18,6 +18,49 @@ logger.setLevel("INFO")
 
 
 class Model(object):
+    @staticmethod
+    def all(repository_id="11", language="nob", corpus_id=None):
+        repo = requests.get(
+            "http://vectors.nlpl.eu/repository/{}.json".format(repository_id)
+        ).json()
+
+        corpora = {}
+        algorithms = {}
+
+        for alg in repo["algorithms"]:
+            algorithms[alg["id"]] = alg
+
+        for corp in repo["corpora"]:
+            corpora[corp["id"]] = corp
+
+        filtered_corpus_ids = [
+            key for key, value in corpora.items() if value["language"] == language
+        ]
+
+        models = [
+            model
+            for model in repo["models"]
+            if model["corpus"][0] in filtered_corpus_ids
+        ]
+
+        if corpus_id:
+            models = [
+                m
+                for m in models
+                if len(m["corpus"]) == 1 and m["corpus"][0] == corpus_id
+            ]
+
+        result = []
+
+        for model in models:
+            model["repository_id"] = repository_id
+            model["corpus"] = [corpora[corpus_id] for corpus_id in model["corpus"]]
+            model["algorithm"] = algorithms[model["algorithm"]]
+
+            result.append(Model(model))
+
+        return result
+
     def __init__(self, attrs, out=None):
         self.attrs = attrs
 
@@ -65,7 +108,7 @@ class Model(object):
         spacy_model_path = self.training_path.joinpath("vectors")
 
         if force or not spacy_model_path.exists():
-            shutil.rmtree(spacy_model_path)
+            shutil.rmtree(spacy_model_path, ignore_errors=True)
 
             cmd = [
                 sys.executable,
@@ -96,13 +139,13 @@ class Model(object):
             str(self.training_path),
             "data/norne-spacy/ud/nob/no-ud-train-ner.json",
             "data/norne-spacy/ud/nob/no-ud-dev-ner.json",
-            # "--vectors",
-            # str(spacy_model_path),
+            "--vectors",
+            str(spacy_model_path),
             "--n-iter",
             "15",
             "--gold-preproc",
-            # "--use-gpu",
-            # "1",
+            "--use-gpu",
+            "1",
         ]
 
         log_path = self.training_path.joinpath("train.log")
@@ -132,31 +175,6 @@ class Model(object):
         )
 
 
-def models(repository_id="11"):
-    repo = requests.get(
-        "http://vectors.nlpl.eu/repository/{}.json".format(repository_id)
-    ).json()
-
-    corpora = {}
-    algorithms = {}
-
-    for alg in repo["algorithms"]:
-        algorithms[alg["id"]] = alg
-
-    for corp in repo["corpora"]:
-        corpora[corp["id"]] = corp
-
-    nob = [key for key, value in corpora.items() if value["language"] == "nob"]
-    models = [model for model in repo["models"] if model["corpus"][0] in nob]
-
-    for model in models:
-        model["repository_id"] = repository_id
-        model["corpus"] = [corpora[corpus_id] for corpus_id in model["corpus"]]
-        model["algorithm"] = algorithms[model["algorithm"]]
-
-        yield model
-
-
 def main(output_dir):
     output_dir = Path(output_dir)
 
@@ -172,9 +190,13 @@ def main(output_dir):
     ]
     # fmt: on
 
-    for model_data in models():
-        m = Model(model_data)
+    NORWEGIAN_NEWS_CORPUS = 79
 
+    models = Model.all(
+        repository_id=11, language="nob", corpus_id=NORWEGIAN_NEWS_CORPUS
+    )
+
+    for m in models:
         if m.id() in IGNORED:
             continue
 
@@ -182,8 +204,6 @@ def main(output_dir):
 
         m.fetch()
         m.train()
-
-        break
 
 
 if __name__ == "__main__":
