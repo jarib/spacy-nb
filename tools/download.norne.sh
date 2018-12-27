598cac6ec9fa48002ab68a75cb673a6167a4d06c
Jari Bakken <jari.bakken@gmail.com>
2018-12-27 16:13:41 +0100

    Pass -n to `spacy convert` and use a larger model as default

--- tools/download.norne.sh
+++ tools/download.norne.sh
@@ -11,6 +11,6 @@ mkdir -p "$OUTPUT_DIR"
 [[ -d "$CHECKOUT_DIR" ]] || git clone git@github.com:ltgoslo/norne.git "$CHECKOUT_DIR"
 
 for f in "$CHECKOUT_DIR"/ud/nob/*.conllu; do
-    python -m spacy convert "$f" "$OUTPUT_DIR" --converter conllu --morphology
+    python -m spacy convert "$f" "$OUTPUT_DIR" -n 10 --converter conllu --morphology
 done
 
--- tools/download.vectors.sh
+++ tools/download.vectors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="http://vectors.nlpl.eu/repository/11/134.zip"
+URL="http://vectors.nlpl.eu/repository/11/100.zip"
 OUT="data/vectors"
 
 wget "$URL" -O "$OUT.zip"
